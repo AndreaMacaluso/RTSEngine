cfe66123fb5f7ede7b1bcd6b52e671c4069d0bdf
AndreaMacaluso <a.macaluso1991@gmail.com>
2026-05-29 22:13:05 +0200

    implemented player base set up

---
+++ RTSEngine.Core/Players/Player.cs
@@ -0,0 +1,9 @@
+namespace RTSEngine.Core.Players;
+public class Player
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = "";
+
+    public ConsoleColor Color { get; set; }
+}

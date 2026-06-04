1cf483443ae3d9bf86a8d7c06f6092634ae29df7
AndreaMacaluso <a.macaluso1991@gmail.com>
2026-06-04 12:31:09 +0200

    Introducing resources on player

--- RTSEngine.Core/Players/Player.cs
+++ RTSEngine.Core/Players/Player.cs
@@ -1,3 +1,4 @@
+using RTSEngine.Core.Map.Runtime;
 namespace RTSEngine.Core.Players;
 public class Player
 {
@@ -6,4 +7,78 @@ public class Player
     public string Name { get; set; } = "";
 
     public ConsoleColor Color { get; set; }
+
+    public int Wood { get; private set; }
+    public int Food { get; private set; }
+    public int Gold { get; private set; }
+    public int Stone { get; private set; }
+
+    public void AddResource(
+    ResourceType type,
+    int amount)
+    {
+        switch(type)
+        {
+            case ResourceType.Wood:
+                Wood += amount;
+                break;
+
+            case ResourceType.Food:
+                Food += amount;
+                break;
+
+            case ResourceType.Gold:
+                Gold += amount;
+                break;
+
+            case ResourceType.Stone:
+                Stone += amount;
+                break;
+        }
+    }
+
+    public bool SpendResource(
+    ResourceType type,
+    int amount)
+    {
+        if (!HasResource(type, amount))
+        {
+            return false;
+        }
+
+        switch(type)
+        {
+            case ResourceType.Wood:
+                Wood -= amount;
+                break;
+
+            case ResourceType.Food:
+                Food -= amount;
+                break;
+
+            case ResourceType.Gold:
+                Gold -= amount;
+                break;
+
+            case ResourceType.Stone:
+                Stone -= amount;
+                break;
+        }
+
+        return true;
+    }
+
+    public bool HasResource(
+    ResourceType type,
+    int amount)
+    {
+        return type switch
+        {
+            ResourceType.Wood => Wood >= amount,
+            ResourceType.Food => Food >= amount,
+            ResourceType.Gold => Gold >= amount,
+            ResourceType.Stone => Stone >= amount,
+            _ => false
+        };
+    }
 }

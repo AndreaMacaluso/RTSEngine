cb73fef0f83d298904099da49f8f4a15519829c6
AndreaMacaluso <a.macaluso1991@gmail.com>
2026-07-12 23:18:11 +0200

    update Comand System and Simulation Runner

--- RTSEngine.Core/Systems/ComandSystem.cs
+++ RTSEngine.Core/Systems/ComandSystem.cs
@@ -31,9 +31,47 @@ public static class CommandSystem
             case GatherCommand gatherCommand:
                 HandleGather(world, gatherCommand);
                 break;
+            case BuildCommand buildCommand:
+                HandleBuild(world, buildCommand);
+                break;
         }
     }
+    private static void HandleBuild(
+    GameWorld world,
+    BuildCommand command)
+    {
+        foreach (var unitId in command.UnitIds)
+        {
+            var unit = world.GetUnitById(unitId);
+            if (unit == null)
+                continue;
 
+            var building = world.GetBuildingById(command.BuildingId);
+
+            if (building == null)
+            {
+                unit.CurrentTask = UnitTask.Idle;
+                continue;
+            }
+           
+            var target = WorldQueries.FindClosestAdjacentWalkableTile(
+                    world,
+                    unit.Position,
+                    building.Position);
+           
+            if (target == null)
+            {
+                unit.CurrentTask = UnitTask.Idle;
+                unit.Build.BuildingId = null;
+                continue;
+            }
+
+            unit.CurrentTask = UnitTask.Building;
+            unit.Build.BuildingId = building.Id;
+            unit.Build.Phase = BuildPhase.MovingToConstruction;
+            AssignMoveTarget(unit, target.Value, world);
+        }
+    }
     private static void HandleGather(
     GameWorld world,
     GatherCommand command)

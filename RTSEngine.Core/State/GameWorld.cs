ee059cf4514eb5e78dfd100d551848c3377cbaf1
AndreaMacaluso <a.macaluso1991@gmail.com>
2026-08-20 22:24:05 +0200

    Refactor Game World

--- RTSEngine.Core/State/GameWorld.cs
+++ RTSEngine.Core/State/GameWorld.cs
@@ -3,12 +3,10 @@ using RTSEngine.Core.Entities;
 using RTSEngine.Core.Players;
 using RTSEngine.Core.Entities.Resources;
 using RTSEngine.Core.Map.Definitions;
-using RTSEngine.Core.Map.Rules;
 using RTSEngine.Core.Commands;
 using RTSEngine.Core.Entities.Units;
 using RTSEngine.Core.Diagnostics;
 using RTSEngine.Core.Entities.Buildings;
-using RTSEngine.Core.Helpers;
 namespace RTSEngine.Core.State;
 
 public class GameWorld
@@ -16,13 +14,23 @@ public class GameWorld
     public TileMap Map { get; }
     public int CurrentTick { get; private set; }
     public WorldState State { get; private set; } = WorldState.Running;
-    public List<Entity> Entities { get; } = [];
-    public List<ResourceNode> Resources { get; } = [];
-    public List<SpawnPointDefinition> Spawns { get; } = [];
-    private int _nextEntityId = 1;
-    public List<Player> Players { get; } = [];
 
-    public Queue<ICommand> PendingCommands { get; } = [];
+    private readonly List<Entity> _entities = [];
+    private readonly List<ResourceNode> _resources = [];
+    private readonly List<SpawnPointDefinition> _spawns = [];
+    private readonly List<Player> _players = [];
+    private readonly Queue<ICommand> _pendingCommands = [];
+
+    public IReadOnlyList<Entity> Entities => _entities;
+    public IReadOnlyList<ResourceNode> Resources => _resources;
+    public IReadOnlyList<SpawnPointDefinition> Spawns => _spawns;
+    public IReadOnlyList<Player> Players => _players;
+    public IReadOnlyCollection<ICommand> PendingCommands => _pendingCommands;
+
+    public IEnumerable<Building> Buildings => _entities.OfType<Building>();
+
+    private int _nextEntityId = 1;
+
     public GameWorld(
         TileMap map,
         List<ResourceNode>? resources = null,
@@ -34,82 +42,21 @@ public class GameWorld
         {
             AddResource(resource);
         }
-        Spawns = spawns ?? [];
-        Entities = [];
+        _spawns.AddRange(spawns ?? []);
         CurrentTick = 0;
     }
+
     public void AdvanceTick()
-    {   
+    {
         CurrentTick++;
         DebugSession.Log.Info($"CurrentTick: {CurrentTick}");
     }
 
     public Entity? GetEntityAt(int x, int y)
     {
-        return Entities.FirstOrDefault(
+        return _entities.FirstOrDefault(
             e => e.Position.X == x
             && e.Position.Y == y);
-    }
-
-    public bool IsResourceAt(int x, int y)
-    {
-        return Resources.Any(
-            r => r.Position.X == x
-            && r.Position.Y == y
-            && !r.IsDepleted);
-    }
-
-    public bool IsBuildingAt(int x, int y)
-    {
-        var pos = new GridPosition(x, y);
-
-        return Entities
-            .OfType<Building>()
-            .Any(b => BuildingQueries.OccupiesTile(
-                b.Definition,
-                b.Position,
-                pos));
-    }
-
-    public bool IsTileOccupied(int x, int y)
-    {
-        return GetEntityAt(x, y) != null;
-    }
-    public bool IsInsideBounds(int x, int y)
-    {
-        return x >= 0
-            && y >= 0
-            && x < Map.Width
-            && y < Map.Height;
-    }
-
-    public bool IsTileBlocked(int x, int y)
-    {
-        if (!IsInsideBounds(x, y))
-        {
-            return true;
-        }
-
-        var tile = Map.GetTile(x, y);
-
-        if (!TileRules.IsWalkable(tile))
-        {
-            return true;
-        }
-
-        if (IsResourceAt(x, y))
-        {
-            return true;
-        }
-
-        if (IsBuildingAt(x, y))
-        {
-            return true;
-        }
-
-        var entity = GetEntityAt(x, y);
-
-        return entity?.IsBlocking ?? false;
     }
 
     public int GenerateEntityId()
@@ -120,18 +67,26 @@ public class GameWorld
     public void AddEntity(Entity entity)
     {
         entity.Id = GenerateEntityId();
-
-        Entities.Add(entity);
+        _entities.Add(entity);
     }
+
     public void AddCommand(ICommand command)
     {
-        PendingCommands.Enqueue(command);
+        _pendingCommands.Enqueue(command);
+    }
+
+    public ICommand? DequeueCommand()
+    {
+        return _pendingCommands.Count > 0
+            ? _pendingCommands.Dequeue()
+            : null;
     }
 
     public void AddPlayer(Player player)
     {
-        Players.Add(player);
+        _players.Add(player);
     }
+
     public void Pause()
     {
         State = WorldState.Paused;
@@ -150,43 +105,41 @@ public class GameWorld
     public void AddResource(ResourceNode resource)
     {
         resource.Id = GenerateEntityId();
-
-        Resources.Add(resource);
+        _resources.Add(resource);
     }
-    
+
     public ResourceNode? GetResourceById(int id)
     {
-        return Resources.FirstOrDefault(r => r.Id == id);
+        return _resources.FirstOrDefault(r => r.Id == id);
     }
+
     public Player? GetPlayerById(int id)
     {
-        return Players.FirstOrDefault(p => p.Id == id);
+        return _players.FirstOrDefault(p => p.Id == id);
     }
+
     public Entity? GetEntityById(int id)
     {
-        return Entities.FirstOrDefault(e => e.Id == id);
+        return _entities.FirstOrDefault(e => e.Id == id);
     }
+
     public Building? GetBuildingById(int id)
     {
         return GetEntityById(id) as Building;
     }
-     public Unit? GetUnitById(int id)
+
+    public Unit? GetUnitById(int id)
     {
         return GetEntityById(id) as Unit;
     }
 
-    public IEnumerable<Building> GetBuildings()
-    {
-        return Entities.OfType<Building>();
-    }
-
     public void RemoveEntity(Entity entity)
     {
-        Entities.Remove(entity);
-    }
-    public void RemoveResources(ResourceNode resourceNode)
-    {
-        Resources.Remove(resourceNode);
+        _entities.Remove(entity);
     }
 
+    public void RemoveResource(ResourceNode resource)
+    {
+        _resources.Remove(resource);
+    }
 }
